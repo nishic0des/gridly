79d45db03cbbb0cc6b8ba5c8fcc0f9326cdf9f66
Nishi <nishi241105@gmail.com>
2025-09-28 21:47:23 +0530

    modified

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	webpack: (config, { isServer }) => {
+		if (isServer) {
+			// Ensure Prisma Client is generated during build
+			require("./prisma/generate-prisma");
+		}
+		return config;
+	},
+};
 
 export default nextConfig;
