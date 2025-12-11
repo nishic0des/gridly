25b7adfd84540722140015275f5e5a6b7e49776e
Nishi <nishi241105@gmail.com>
2025-12-11 08:40:43 +0530

    linting fix

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,12 +1,10 @@
 "use client";
-import { SignUp } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
+import { SignUp as _SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
-	const router = useRouter();
 	return (
 		<div className="flex justify-center items-center min-h-screen">
-			<SignUp afterSignInUrl="/spreadsheets" afterSignUpUrl="/spreadsheets" />
+			<_SignUp afterSignInUrl="/spreadsheets" afterSignUpUrl="/spreadsheets" />
 		</div>
 	);
 }
